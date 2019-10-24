bd233dd09e279a1acf3e76ce7fda93e2ce0c4e77
qiancheng <qian273016@163.com>
2019-10-24 15:50:26 +0800

    Initial commit

---
+++ scripts/test02.py
@@ -0,0 +1,28 @@
+import allure
+import pytest
+
+
+class Test02:
+    @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
+    @allure.step(title="执行登录")
+    def test01(self):
+        assert 0
+        allure.attach("失败原因：","验证码错误！")
+        pass
+
+    @pytest.allure.severity(pytest.allure.severity_level.MINOR)
+    @allure.step(title="执行退出登录")
+    def test02(self):
+        allure.attach("失败原因：","密码错误！")
+        pass
+
+    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
+    def test03(self):
+        allure.attach("失败原因：","密码错误！")
+        pass
+
+    @pytest.allure.severity(pytest.allure.severity_level.TRIVIAL)
+    def test04(self):
+        assert 0
+        pass
+
